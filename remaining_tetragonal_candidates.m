3d4bcc1370c5e2622fbc227bc6689ba86d296e2c
fsaia <freddy.v.saia@gmail.com>
2025-11-03 19:01:20 -0600

    Update remaining_tetragonal_candidates.m

--- remaining_tetragonal_candidates.m
+++ remaining_tetragonal_candidates.m
@@ -1,4 +1,4 @@
-// All 32 pairs (D,N) for which we remain unsure of whether X_0^D(N) is tetragonal over \Q.
+// All 27 pairs (D,N) for which we remain unsure of whether X_0^D(N) is tetragonal over \Q.
 // This comes from further narrowing the pairs in tetragonal_top_candidates.m
 // by identifying tetragonal curves (using past work on curve of gonality up to 2 and
 // CM points) and excluding candidates which are not geometrically tetragonal. 
@@ -6,7 +6,6 @@
 remaining_tetragonal_candidates := 
 [
     [ 6, 49 ],
-    [ 6, 73 ],
     [ 10, 27 ],
     [ 14, 9 ],
     [ 15, 8 ],
@@ -21,21 +20,17 @@ remaining_tetragonal_candidates :=
     [ 46, 9 ],
     [ 51, 5 ],
     [ 55, 2 ],
-    [ 55, 3 ],
-    [ 69, 2 ],
     [ 87, 2 ],
     [ 94, 3 ],
     [ 95, 2 ],
     [ 111, 2 ],
     [ 133, 1 ],
     [ 177, 1 ],
-    [ 187, 1 ],
     [ 213, 1 ],
     [ 214, 1 ],
     [ 226, 1 ],
     [ 262, 1 ],
     [ 298, 1 ],
     [ 358, 1 ],
-    [ 382, 1 ]
 ]
 ;
