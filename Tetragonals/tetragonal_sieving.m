c0f3c0da1fbe17e700e8574392319b2eb06965db
fsaia <freddy.v.saia@gmail.com>
2026-05-22 14:48:48 -0400

    Removing commented code

--- Tetragonals/tetragonal_sieving.m
+++ Tetragonals/tetragonal_sieving.m
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:00c1d2c7c2ae451d142eeae600b1d06d939dd929f94b89d776f226a071879cb5
-size 42261
+oid sha256:d5b598b30be4c182b4b827265047a2a39301d8daf23551f1cde166b1b46a1369
+size 33854
