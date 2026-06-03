87195f2d7fc4000a0d35ee1e9ff093e114a8d6bd
fsaia <freddy.v.saia@gmail.com>
2026-06-03 10:05:11 -0500

    added line loading point counts code

--- Point Counts/examples.m
+++ Point Counts/examples.m
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f7b439c7974b55b14bd3bcbda840d8fd09ae8203d4e3c091899bf32f94517dce
-size 2760
+oid sha256:ac323d3fb85a30fb803823019c01bf2609cfa6b17acefad56980e248c01384e7
+size 2848
